98135c38ba7faf9f199bae7bcdee1f69116faa38
Yatin Lala <yatin.lala@gmail.com>
2022-06-02 20:43:08 -0400

    Renamed statusline.lua to lualine.lua

--- nvim/.config/nvim/lua/plugin/init.lua
+++ nvim/.config/nvim/lua/plugin/init.lua
@@ -15,7 +15,7 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true },
         config = function()
-            require('plugin.config.statusline')
+            require('plugin.config.lualine')
         end,
     }
     -- Lua
