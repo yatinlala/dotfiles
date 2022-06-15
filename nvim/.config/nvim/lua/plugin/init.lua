3057b5c8b484b44d03e459a38561304a81fead94
Yatin Lala <yatin.lala@gmail.com>
2022-06-14 21:58:34 -0400

    Added vim-cool

--- nvim/.config/nvim/lua/plugin/init.lua
+++ nvim/.config/nvim/lua/plugin/init.lua
@@ -1,7 +1,7 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-    packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    Packer_Bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
 end
 
 return require('packer').startup(function(use)
@@ -34,11 +34,6 @@ return require('packer').startup(function(use)
     -- }
 
     -- VISUAL
-    -- use { 'akinsho/bufferline.nvim',
-    --     config = function()
-    --         require('plugin.config.bufferline')
-    --     end,
-    -- }
     use { 'folke/which-key.nvim',
         config = function()
             require('plugin.config.whichkey')
@@ -194,6 +189,7 @@ return require('packer').startup(function(use)
         end,
         cmd = 'ToggleTerm',
     }
+    use { 'romainl/vim-cool' }
     use {
         'chipsenkbeil/distant.nvim',
         config = function()
@@ -204,7 +200,7 @@ return require('packer').startup(function(use)
         cmd = 'DistantLaunch'
     }
     use { 'wbthomason/packer.nvim', }
-    if packer_bootstrap then
+    if Packer_Bootstrap then
         require('packer').sync()
     end
 end)
