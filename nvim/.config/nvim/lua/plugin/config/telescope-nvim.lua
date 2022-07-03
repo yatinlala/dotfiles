406e1b0e59208e345a04c1f3607a7541636d9f8f
Yatin Lala <yatin.lala@gmail.com>
2022-07-03 12:07:56 -0400

    Added trouble to telescope

--- nvim/.config/nvim/lua/plugin/config/telescope-nvim.lua
+++ nvim/.config/nvim/lua/plugin/config/telescope-nvim.lua
@@ -1,25 +1,32 @@
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
-  return
+    return
 end
 
-require("telescope").setup {
-  defaults = {
+local trouble = require("trouble.providers.telescope")
+local telescope = require("telescope")
 
-    prompt_prefix = " ",
-    selection_caret = " ",
-    path_display = { "smart" },
-      file_sorter = require('telescope.sorters').get_fzy_sorter,
-      color_devicons = true
+telescope.setup {
+    defaults = {
+        prompt_prefix = " ",
+        selection_caret = " ",
+        path_display = { "smart" },
+        file_sorter = require('telescope.sorters').get_fzy_sorter,
+        color_devicons = true
+    },
+    pickers = {
+        find_files = {
+            follow = true
+        }
+    },
+    extensions = {
+    },
+    mappings = {
+        i = { ["<c-t>"] = trouble.open_with_trouble },
+        n = { ["<c-t>"] = trouble.open_with_trouble },
 
-  },
-  pickers = {
-    find_files = {
-        follow = true
-    }
-  },
-  extensions = {
-  }
+
+    },
 }
 require('telescope').load_extension('fzy_native')
 require('telescope').load_extension('projects')
@@ -29,7 +36,7 @@ M.search_dotfiles = function()
     require('telescope.builtin').find_files({
         prompt_title = "< vimrc >",
         cwd = "~/.dotfiles/nvim/.config/nvim/",
-        })
+    })
 end
 
 return M
