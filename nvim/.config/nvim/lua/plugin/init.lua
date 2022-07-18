f17af0016f614a65e99ad071eb44937a42474a8a
Yatin Lala <yatin.lala@gmail.com>
2022-07-18 10:52:34 -0400

    Installed nvim-orgmode

--- nvim/.config/nvim/lua/plugin/init.lua
+++ nvim/.config/nvim/lua/plugin/init.lua
@@ -189,27 +189,28 @@ return require('packer').startup(function(use)
             vim.api.nvim_set_keymap("n", "gR", "<cmd>Trouble lsp_references<cr>", { silent = true, noremap = true })
         end
     }
-    -- use { 'nvim-orgmode/orgmode',
-    --     config = function()
-    --         -- Load custom tree-sitter grammar for org filetype
-    --         require('orgmode').setup_ts_grammar()
-    --
-    --         -- Tree-sitter configuration
-    --         require 'nvim-treesitter.configs'.setup {
-    --             -- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
-    --             highlight = {
-    --                 enable = true,
-    --                 additional_vim_regex_highlighting = { 'org' }, -- Required for spellcheck, some LaTex highlights and code block highlights that do not have ts grammar
-    --             },
-    --             ensure_installed = { 'org' }, -- Or run :TSUpdate org
-    --         }
-    --
-    --         require('orgmode').setup({
-    --             org_agenda_files = { '~/documents/org/*', '~/documents/org/my-orgs/**/*' },
-    --             org_default_notes_file = '~/documents/org/refile.org',
-    --         })
-    --     end
-    -- }
+    use { 'nvim-orgmode/orgmode',
+        config = function()
+            -- Load custom tree-sitter grammar for org filetype
+            require('orgmode').setup_ts_grammar()
+
+            -- Tree-sitter configuration
+            require 'nvim-treesitter.configs'.setup {
+                -- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = { 'org' }, -- Required for spellcheck, some LaTex highlights and code block highlights that do not have ts grammar
+                },
+                ensure_installed = { 'org' }, -- Or run :TSUpdate org
+            }
+
+            require('orgmode').setup({
+                org_agenda_files = { '~/documents/org/*', '~/documents/org/my-orgs/**/*' },
+                org_default_notes_file = '~/documents/org/refile.org',
+            })
+        end,
+        requires = 'nvim-treesitter/nvim-treesitter'
+    }
     use { 'akinsho/toggleterm.nvim',
         config = function()
             require('plugin.config.toggleterm')
