6b74572e163bf04933ecf317d0740564eb3866ab
Yatin Lala <yatin.lala@gmail.com>
2023-02-13 16:59:12 -0500

    add luasnip

--- nvim/.config/nvim/lua/config/plugins/init.lua
+++ nvim/.config/nvim/lua/config/plugins/init.lua
@@ -71,6 +71,13 @@ return {
             })
         end,
     },
+    {
+        "L3MON4D3/LuaSnip",
+        dependencies = { "rafamadriz/friendly-snippets" },
+        config = function()
+            require("luasnip.loaders.from_vscode").lazy_load()
+        end,
+    },
     -- {
     --     "Exafunction/codeium.vim",
     --     cmd = "Codeium",
