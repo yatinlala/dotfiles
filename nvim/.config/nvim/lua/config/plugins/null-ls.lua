8f6356c006e8ea404fe313a522a4e86b694b2b54
Yatin Lala <yatin.lala@gmail.com>
2023-02-22 14:37:21 -0500

    add shfmt to null-ls sources

--- nvim/.config/nvim/lua/config/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/config/plugins/null-ls.lua
@@ -1,23 +1,21 @@
 local M = {
-    "jose-elias-alvarez/null-ls.nvim",
-    dependencies = {
-        "neovim/nvim-lspconfig",
-    },
-    event = "VeryLazy",
+	"jose-elias-alvarez/null-ls.nvim",
+	dependencies = {
+		"neovim/nvim-lspconfig",
+	},
+	event = "VeryLazy",
 }
 
 function M.config()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-        sources = {
-            null_ls.builtins.formatting.stylua,
-            null_ls.builtins.formatting.ocamlformat.with({
-                extra_args = { "--enable-outside-detected-project" },
-            }),
-            null_ls.builtins.diagnostics.eslint,
-            null_ls.builtins.completion.spell,
-        },
-    })
+	local null_ls = require("null-ls")
+	null_ls.setup({
+		sources = {
+			null_ls.builtins.formatting.stylua,
+			null_ls.builtins.formatting.shfmt,
+			null_ls.builtins.diagnostics.eslint,
+			null_ls.builtins.completion.spell,
+		},
+	})
 end
 
 return M
