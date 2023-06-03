0a040b829b41cc7a835340b2262e3e9e590a5102
Yatin Lala <yatin.lala@gmail.com>
2023-06-03 11:39:25 -0700

    nvim gruvbox no longer italicizes strings or comments

--- nvim/.config/nvim/lua/config/plugins/init.lua
+++ nvim/.config/nvim/lua/config/plugins/init.lua
@@ -5,6 +5,29 @@ return {
 		lazy = false,
 		priority = 1000,
 		config = function(_, _)
+			-- Defaults are commented out
+			require("gruvbox").setup({
+				-- undercurl = true,
+				-- underline = true,
+				-- bold = true,
+				italic = {
+					strings = false,
+					comments = false,
+					-- operators = false,
+					-- folds = true,
+				},
+				-- strikethrough = true,
+				-- invert_selection = false,
+				-- invert_signs = false,
+				-- invert_tabline = false,
+				-- invert_intend_guides = false,
+				-- inverse = true, -- invert background for search, diffs, statuslines and errors
+				-- contrast = "", -- can be "hard", "soft" or empty string
+				-- palette_overrides = {},
+				-- overrides = {},
+				-- dim_inactive = false,
+				-- transparent_mode = false,
+			})
 			vim.cmd.colorscheme("gruvbox")
 		end,
 	},
@@ -55,15 +78,13 @@ return {
 		"utilyre/barbecue.nvim",
 		name = "barbecue",
 		version = "*",
-		event = "VeryLazy",
+		-- event = "VeryLazy",
 		dependencies = {
 			"SmiteshP/nvim-navic",
 			"kyazdani42/nvim-web-devicons", -- optional dependency
 		},
-		opts = {
-			-- configurations go here
-		},
 	},
+	-- { "Bekaboo/dropbar.nvim", lazy = false },
 	{
 		"lukas-reineke/indent-blankline.nvim",
 		event = "BufWinEnter",
