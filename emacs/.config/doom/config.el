ce23948cf6b594c99ecd947084291fe8935484fd
Yatin Lala <yatin.lala@gmail.com>
2023-06-18 09:13:06 -0700

    add neomutt and gamemode; rework keybindings

--- emacs/.config/doom/config.el
+++ emacs/.config/doom/config.el
@@ -75,19 +75,5 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
-(set-email-account!
- "gmail"
- '((mu4e-sent-folder       . "/[Gmail]/Sent Mail")
-   (mu4e-trash-folder      . "/[Gmail]/Trash")
-   (smtpmail-smtp-user     . "yatin.lala@gmail.com"))
- t)
-(setq mu4e-get-mail-command "mbsync -c ~/.config/mbsyncrc gmail"
-      ;; get emails and index every 5 minutes
-      mu4e-update-interval 300
-	  ;; send emails with format=flowed
-	  mu4e-compose-format-flowed t
-	  ;; no need to run cleanup after indexing for gmail
-	  mu4e-index-cleanup nil
-	  mu4e-index-lazy-check t
-      ;; -more- sensible date format
-      mu4e-headers-date-format "%m.%d.%y")
+
+(setq org-startup-folded t)
