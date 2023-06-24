99bc09008e67e81786cfbd07464b28c5f44ae799
Yatin Lala <yatin.lala@gmail.com>
2023-06-23 23:33:56 -0700

    make gamemode-lite liter

--- hypr/.config/hypr/gamemode-lite.sh
+++ hypr/.config/hypr/gamemode-lite.sh
@@ -4,10 +4,10 @@ if [ "$HYPRGAMEMODE" = 1 ] ; then
     hyprctl --batch "\
         keyword animations:enabled 0;\
         keyword decoration:drop_shadow 0;\
-        keyword general:gaps_in 0;\
-        keyword general:gaps_out 0;\
-        keyword general:border_size 2;\
-        keyword decoration:rounding 0"
+        # keyword general:gaps_in 0;\
+        # keyword general:gaps_out 0;\
+        # keyword general:border_size 2;\
+        # keyword decoration:rounding 0"
     exit
 fi
 hyprctl reload
