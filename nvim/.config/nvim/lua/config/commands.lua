c5d56873af106bce74f32f21dbcc7dfc9d764ab9
Yatin Lala <yatin.lala@gmail.com>
2023-07-09 21:19:32 -0700

    add opengithubrepo command

--- nvim/.config/nvim/lua/config/commands.lua
+++ nvim/.config/nvim/lua/config/commands.lua
@@ -1,3 +1,15 @@
-vim.api.nvim_create_user_command('ToggleBackground', require('util').toggleBg, {})
 vim.cmd("command! -nargs=? -complete=dir Lf :lua require('util').Lf(<f-args>)")
+
 vim.cmd("command! -nargs=? -complete=dir Lazygit :lua require('util').Lazygit(<f-args>)")
+
+vim.api.nvim_create_user_command('ToggleBackground', require('util').toggleBg, {})
+
+vim.api.nvim_create_user_command("OpenGithubRepo", function(_)
+  local ghpath = vim.api.nvim_eval("shellescape(expand('<cfile>'))")
+  local formatpath = ghpath:sub(2, #ghpath - 1)
+  local repourl = "https://www.github.com/" .. formatpath
+  vim.fn.system({ "xdg-open", repourl })
+end, {
+  desc = "Open Github Repo",
+  force = true,
+})
