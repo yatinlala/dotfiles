e5e65402b8ca15333dbd94e6416a802bd5d6c0be
Yatin Lala <yatin.lala@gmail.com>
2023-12-16 16:35:37 -0500

    switch to sf mono

--- emacs/.config/doom/config.el
+++ emacs/.config/doom/config.el
@@ -21,8 +21,8 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "BlexMono Nerd Font" :size 15)
-     doom-variable-pitch-font (font-spec :family "BlexMono Nerd Font" :size 15))
+(setq doom-font (font-spec :family "Liga SFMono Nerd Font" :size 15)
+     doom-variable-pitch-font (font-spec :family "Liga SFMono Nerd Font" :size 15))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
