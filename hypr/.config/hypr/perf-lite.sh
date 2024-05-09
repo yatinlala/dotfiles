036a90a91dc410032bbab2fd5197933e032d5dd6
Yatin Lala <yatin.lala@gmail.com>
2024-05-09 19:24:14 -0400

    simplify and fix perf script

--- hypr/.config/hypr/perf-lite.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==2{print $2}')
-if [ "$HYPRGAMEMODE" = 1 ] ; then
-    hyprctl --batch "\
-        keyword decoration:drop_shadow 0;\
-        keyword animations:enabled 0"
-    exit
-fi
-hyprctl reload
--- hypr/.config/hypr/perf.sh
+++ hypr/.config/hypr/perf.sh
@@ -1,16 +1,11 @@
-#!/usr/bin/env sh
-HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==2{print $2}')
-if [ "$HYPRGAMEMODE" = 1 ] ; then
-    hyprctl --batch "\
-        keyword animations:enabled 0;\
-        keyword decoration:drop_shadow 0;\
-        keyword general:gaps_in 0;\
-        keyword general:gaps_out 0;\
-        keyword general:border_size 1;\
-        keyword decoration:rounding 0;\
-        keyword decoration:blur:enabled 0;\
-        keyword decoration:active_opacity 1.0;\
-        keyword decoration:inactive_opacity 1.0;"
-    exit
+#!/usr/bin/env bash
+
+on=$(hyprctl -j getoption animations:enabled | jq --raw-output '.int')
+
+if [[ $on -eq 1 ]]; then
+    hyprctl keyword animations:enabled 0
+    hyprctl notify -1 1000 "rgb(e06c75)" "Animations off"
+else
+    hyprctl keyword animations:enabled 1
+    hyprctl notify -1 1000 "rgb(98c379)" "Animations on"
 fi
-hyprctl reload
