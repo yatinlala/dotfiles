a39d6eb7e9cf5440229cd1fd86877d207b2ccc83
Yatin Lala <yatin.lala@gmail.com>
2024-07-17 09:25:52 -0700

    switch to jetbrains

--- nvim/.config/nvim/lua/custom/plugins/whichkey.lua
+++ nvim/.config/nvim/lua/custom/plugins/whichkey.lua
@@ -4,17 +4,18 @@ return {
     config = function()
         local wk = require('which-key')
 
+        wk.setup({ preset = 'helix' })
+
         wk.add({
             { '<leader>f', group = '[F]ind' }, -- group
-            { '<leader>g', group = '[G]it' }, -- group
+            { '<leader>g', group = '[G]it' },  -- group
             { '<leader>o', group = '[O]rg Mode' }, -- group
-            { '<leader>l', group = '[L]sp' }, -- group
+            { '<leader>l', group = '[L]sp' },  -- group
             { '<leader>t', group = '[T]reesitter' }, -- group
-        --     ['<leader>d'] = { name = 'document', _ = 'which_key_ignore' },
-        --     ['<leader>r'] = { name = 'rename', _ = 'which_key_ignore' },
-        --     ['<leader>l'] = { name = 'lsp', _ = 'which_key_ignore' },
-        --     ['<leader>w'] = { name = 'workspace', _ = 'which_key_ignore' },
+            --     ['<leader>d'] = { name = 'document', _ = 'which_key_ignore' },
+            --     ['<leader>r'] = { name = 'rename', _ = 'which_key_ignore' },
+            --     ['<leader>l'] = { name = 'lsp', _ = 'which_key_ignore' },
+            --     ['<leader>w'] = { name = 'workspace', _ = 'which_key_ignore' },
         })
-
     end,
 }
