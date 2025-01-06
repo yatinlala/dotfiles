74ff23f1b13e403dcb8cf6183e24c1e49b7d196b
Yatin Lala <yatin.lala@gmail.com>
2025-01-05 23:23:35 -0800

    add g.zsh

---
+++ config/zsh/dot-config/zsh/conf.d/g.zsh
@@ -0,0 +1,31 @@
+g() { 
+  EXTRACTED_JUMPS=$(grep -E "map .* cd .*" ~/.config/lf/lfrc | sed "s/map g//" | sed "s/ cd//")
+
+  if [ -z "$1" ]; then
+    echo "Usage: $0 <acronym>"
+   return 1
+  fi
+
+  ACRONYM="$1"
+
+  MATCH=$(echo $EXTRACTED_JUMPS | grep -E "^$ACRONYM\s+")
+
+  if [ -z "$MATCH" ]; then
+    echo "No matching acronym found."
+    return 1
+  fi
+
+  DIR=$(echo "$MATCH" | awk '{print $2}')
+
+  # Expand ~ to the user's home directory if present.
+  DIR="${DIR/#\~/$HOME}"
+
+  # Check if the directory exists.
+  if [ ! -d "$DIR" ]; then
+    echo "Directory $DIR does not exist."
+    return 1
+  fi
+
+  echo "✨"
+  cd "$DIR" || return 1
+}
