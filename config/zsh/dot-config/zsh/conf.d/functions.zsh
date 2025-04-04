b137a0727af58a6f622d90576f6aba4515466f47
Yatin Lala <yatin.lala@gmail.com>
2025-04-04 12:51:21 -0700

    switch to focus.go

--- config/zsh/dot-config/zsh/conf.d/functions.zsh
+++ config/zsh/dot-config/zsh/conf.d/functions.zsh
@@ -76,13 +76,12 @@ update-pkglist() {
     flatpak list --app > ~/.dotfiles/pkglist/flatpak
 }
 
- # stable-diffusion() {
- #     cd ~/code/stable-diffusion/stable-diffusion-webui/
- #     export PYTORCH_TRACING_MODE=TORCHFX
- #     export COMMANDLINE_ARGS="--skip-torch-cuda-test --precision full --no-half" 
- #     source sd_env/bin/activate
- #     ./webui.sh
- # }
+ stable-diffusion() {
+     cd ~/code/stable-diffusion/stable-diffusion-webui/
+     export PYTORCH_TRACING_MODE=TORCHFX
+     export COMMANDLINE_ARGS="--skip-torch-cuda-test --precision full --no-half" 
+     source .venv/bin/activate
+ }
 
 
 #Docker
