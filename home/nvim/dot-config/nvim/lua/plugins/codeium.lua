09c68cf3f736a01cd904b8b027386c8b831c7f78
Yatin Lala <yatin.lala@gmail.com>
2025-11-17 17:08:14 -0800

    switch to neocodeium

--- home/nvim/dot-config/nvim/lua/plugins/codeium.lua
+++ home/nvim/dot-config/nvim/lua/plugins/codeium.lua
@@ -1,44 +1,64 @@
-if true then
-    return
-end
-return {
-    "Exafunction/codeium.nvim",
-    event = "VeryLazy",
-    -- main = 'codeium',
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-    keys = {
-        {
-            "<M-]>",
-            function()
-                require("codeium.virtual_text").cycle_or_complete()
-            end,
-            mode = "i",
-            desc = "Codeium Complete/Cycle",
-        },
-    },
-    opts = {
-        enable_cmp_source = false,
-        virtual_text = {
-            enabled = true,
-            manual = true,
-            map_keys = true,
-            -- Key bindings for managing completions in virtual text mode.
-            key_bindings = {
-                -- Accept the current completion.
-                accept = "<Tab>",
-                -- Accept the next word.
-                accept_word = false,
-                -- Accept the next line.
-                accept_line = false,
-                -- Clear the virtual text.
-                clear = "<M-c>",
-                -- Cycle to the next completion.
-                next = false,
-                -- Cycle to the previous completion.
-                prev = "<M-[>",
-            },
-        },
-    },
-}
+vim.pack.add({ "https://github.com/monkoose/neocodeium" })
+
+local neocodeium = require("neocodeium")
+local cmds = require("neocodeium.commands")
+
+require("neocodeium").setup({
+    enabled = false,
+    -- bin = "$HOME/.local/xdg-launch/.codeium/bin/1.46.3/language_server_linux_x64",
+})
+
+vim.keymap.set("i", "<c-i>", function()
+    neocodeium.accept()
+end, { desc = "Accept Codeium Suggestion" })
+vim.keymap.set("n", "<leader>ae", function()
+    cmds.enable()
+end, { desc = "Enable Codeium" })
+vim.keymap.set("n", "<leader>ad", function()
+    cmds.disable()
+end, { desc = "Disable Codeium" })
+
+-- if true then
+--     return
+-- end
+-- return {
+--     "Exafunction/codeium.nvim",
+--     event = "VeryLazy",
+--     -- main = 'codeium',
+--     dependencies = {
+--         "nvim-lua/plenary.nvim",
+--     },
+--     keys = {
+--         {
+--             "<M-]>",
+--             function()
+--                 require("codeium.virtual_text").cycle_or_complete()
+--             end,
+--             mode = "i",
+--             desc = "Codeium Complete/Cycle",
+--         },
+--     },
+--     opts = {
+--         enable_cmp_source = false,
+--         virtual_text = {
+--             enabled = true,
+--             manual = true,
+--             map_keys = true,
+--             -- Key bindings for managing completions in virtual text mode.
+--             key_bindings = {
+--                 -- Accept the current completion.
+--                 accept = "<Tab>",
+--                 -- Accept the next word.
+--                 accept_word = false,
+--                 -- Accept the next line.
+--                 accept_line = false,
+--                 -- Clear the virtual text.
+--                 clear = "<M-c>",
+--                 -- Cycle to the next completion.
+--                 next = false,
+--                 -- Cycle to the previous completion.
+--                 prev = "<M-[>",
+--             },
+--         },
+--     },
+-- }
